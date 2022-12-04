f049c1de79b711ae94ab7c33587b19937a54dbb0
Raj-tech11 <rajtech864@gmail.com>
2022-12-04 15:59:23 +0530

    c

--- packages/sc-button/dist/Button.js
+++ packages/sc-button/dist/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
 const Button = ({ onClick, children, disabled }) => (React.createElement(React.Fragment, null,
-    React.createElement("button", { disabled: disabled, onClick: onClick }, children || "tea")));
+    React.createElement("button", { disabled: disabled, onClick: onClick }, children || "tea-test")));
 export default Button;
 //# sourceMappingURL=Button.js.map
